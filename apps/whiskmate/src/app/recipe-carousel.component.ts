a484fe48b78d7b67896c0bd4c8447598c4514b13
Younes Jaaidi <yjaaidi@gmail.com>
2021-11-03 15:57:50 +0100

    🛠 migrate carousel to a reactive approach

--- apps/whiskmate/src/app/recipe-carousel.component.ts
+++ apps/whiskmate/src/app/recipe-carousel.component.ts
@@ -1,21 +1,44 @@
 import { CommonModule } from '@angular/common';
-import { Component, NgModule, OnInit } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  NgModule,
+  OnDestroy,
+} from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
-import { Recipe } from './recipe/recipe';
+import {
+  BehaviorSubject,
+  combineLatest,
+  map,
+  shareReplay,
+  Subject,
+  takeUntil,
+} from 'rxjs';
 import { RecipePreviewModule } from './recipe/recipe-preview.component';
 import { RecipeRepository } from './recipe/recipe-repository.service';
 
 @Component({
+  changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'wm-recipe-carousel',
   template: `<div class="container">
-      <wm-recipe-preview *ngIf="getRecipe() as recipe" [recipe]="recipe">
+      <wm-recipe-preview *ngIf="recipe$ | async as recipe" [recipe]="recipe">
       </wm-recipe-preview>
     </div>
-    <div *ngIf="recipes">
-      <button [disabled]="!hasPrevious()" mat-button (click)="previous()">
+    <div *ngIf="recipes$ | async">
+      <button
+        [disabled]="(hasPrevious$ | async) !== true"
+        mat-button
+        (click)="previous()"
+      >
         PREVIOUS
       </button>
-      <button [disabled]="!hasNext()" mat-button (click)="next()">NEXT</button>
+      <button
+        [disabled]="(hasNext$ | async) !== true"
+        mat-button
+        (click)="next()"
+      >
+        NEXT
+      </button>
     </div> `,
   styles: [
     `
@@ -30,36 +53,35 @@ import { RecipeRepository } from './recipe/recipe-repository.service';
     `,
   ],
 })
-export class RecipeCarouselComponent implements OnInit {
-  recipeIndex = 0;
-  recipes?: Recipe[];
+export class RecipeCarouselComponent implements OnDestroy {
+  destroyed$ = new Subject();
+
+  recipeIndex$ = new BehaviorSubject(0);
+  recipes$ = this._recipeRepository
+    .getRecipes()
+    .pipe(takeUntil(this.destroyed$), shareReplay(1));
+
+  recipe$ = combineLatest([this.recipes$, this.recipeIndex$]).pipe(
+    map(([recipes, index]) => recipes?.[index])
+  );
+  hasPrevious$ = this.recipeIndex$.pipe(map((index) => index > 0));
+  hasNext$ = combineLatest([this.recipes$, this.recipeIndex$]).pipe(
+    map(([recipes, index]) => index + 1 < recipes.length)
+  );
 
   constructor(private _recipeRepository: RecipeRepository) {}
 
-  ngOnInit() {
-    this._recipeRepository
-      .getRecipes()
-      .subscribe((recipes) => (this.recipes = recipes));
+  ngOnDestroy() {
+    this.destroyed$.next(undefined);
+    this.destroyed$.complete();
   }
 
   next() {
-    this.recipeIndex++;
+    this.recipeIndex$.next(this.recipeIndex$.value + 1);
   }
 
   previous() {
-    this.recipeIndex--;
-  }
-
-  getRecipe() {
-    return this.recipes?.[this.recipeIndex];
-  }
-
-  hasPrevious() {
-    return this.recipeIndex > 0;
-  }
-
-  hasNext() {
-    return this.recipeIndex + 1 < (this.recipes?.length ?? 0);
+    this.recipeIndex$.next(this.recipeIndex$.value - 1);
   }
 }
 
