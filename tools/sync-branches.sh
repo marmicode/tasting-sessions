bcbfce8397667c16d7c4d35ec3f96f4fc7e82930
Younes Jaaidi <yjaaidi@gmail.com>
2021-11-03 13:22:34 +0100

    🛠 update branch list

--- tools/sync-branches.sh
+++ tools/sync-branches.sh
@@ -27,27 +27,4 @@ function cascade_changes() {
 }
 
 cascade_changes \
-  testing-00-boilerplate \
-  testing-01-meal-planner \
-  testing-02-meal-planner-reactive \
-  testing-03-recipe-search-boilerplate \
-  testing-04-recipe-search-isolated-boilerplate \
-  testing-05-recipe-search-isolated \
-  testing-06-recipe-search-integration-boilerplate \
-  testing-07-recipe-search-integration \
-  testing-08-recipe-search-shallow-boilerplate \
-  testing-09-recipe-search-shallow \
-  testing-10-recipe-search-async-pipe \
-  testing-11-recipe-filter-boilerplate \
-  testing-12-recipe-filter \
-  testing-13-recipe-search-filter-interaction \
-  testing-14-recipe-search-add-button \
-  testing-15-recipe-filter-material \
-  testing-16-recipe-filter-material-harness \
-  testing-17-recipe-filter-harness \
-  testing-18-recipe-search-harness \
-  testing-19-recipe-search-harness-testing \
-  testing-20-cypress-component-testing-boilerplate \
-  testing-21-cypress-component-testing \
-  testing-22-cypress-component-testing-with-fake-repository \
-  testing-23-visual-regression-testing \
+  boilerplate
